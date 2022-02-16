739cccb8224bef6171457f3ba870bf7792b35f12
Luis Cardona <cardona-luis@outlook.com>
2022-02-16 06:11:44 -0600

    ContainerLocationUpdated

--- client/src/views/LandingPage.jsx
+++ client/src/views/LandingPage.jsx
@@ -6,10 +6,16 @@ import Services from '../components/LandingPage/Services'
 function LandingPage(props) {
   return (
     <div className='LandingPage-Container'>
+
+      <div>
         <HeroBanner></HeroBanner>
+      </div>
+      <div>
         <LearnToCode></LearnToCode>
+      </div>
+      <div>
         <Services></Services>
-        
+      </div>
         
     </div>
   )
