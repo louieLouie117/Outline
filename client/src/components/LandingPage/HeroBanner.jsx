6ef15ed1ea2ee6cd02095cb5f0724635ff60e3b9
Luis Cardona <cardona-luis@outlook.com>
2022-02-25 07:00:47 -0600

    FixManifestAndLogo

--- client/src/components/LandingPage/HeroBanner.jsx
+++ client/src/components/LandingPage/HeroBanner.jsx
@@ -15,8 +15,8 @@ function HeroBanner(props) {
 
       <svg xmlns="http://www.w3.org/2000/svg" width="215" height="48" viewBox="0 0 115 48">
   <g id="Group_440" data-name="Group 440" transform="translate(-175 -605)">
-    <text id="outline" transform="translate(175 640)" font-size="41" font-family="MS-UIGothic, MS UI Gothic"><tspan x="0" y="0">outline</tspan></text>
-    <g id="Rectangle_156" data-name="Rectangle 156" transform="translate(233 646)" fill="#136dc0" stroke="#fff" stroke-width="0.5">
+    <text id="outline" transform="translate(175 640)" fontSize="41" fontFamily="MS-UIGothic, MS UI Gothic"><tspan x="0" y="0">outline</tspan></text>
+    <g id="Rectangle_156" data-name="Rectangle 156" transform="translate(235 646)" fill="#136dc0" stroke="#fff" strokeWidth="0.5">
       <rect width="56" height="7" rx="3.5" stroke="none"/>
       <rect x="0.25" y="0.25" width="55.5" height="6.5" rx="3.25" fill="none"/>
     </g>
