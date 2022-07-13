349d73c16e5a36c2d77ae05c2d84c0993cd5d7f9
Luis Cardona <cardona-luis@outlook.com>
2022-07-13 07:49:58 -0500

    EstimateComponnet

--- client/src/components/LandingPage/Services.jsx
+++ client/src/components/LandingPage/Services.jsx
@@ -1,7 +1,12 @@
 import React from 'react'
+import Estimate from '../Estimate'
 import Subscription from '../Subscription'
 
 function Services(props) {
+
+
+
+  
   return (
     <div className='Service-Container'>
         <header>
@@ -13,6 +18,8 @@ function Services(props) {
           <h2>Services</h2>
           <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Cumque laborum consectetur distinctio neque provident? Accusantium saepe, fuga delectus explicabo inventore libero iste quod fugit assumenda aperiam quidem provident vitae sapiente!</p>
         </header>
+
+        <Estimate></Estimate>
 
         {/* <Subscription></Subscription> */}
 
