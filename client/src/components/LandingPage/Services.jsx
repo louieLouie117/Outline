10c38b56fbb22e4bf74379ce9ec0457241fa45e6
Luis Cardona <cardona-luis@outlook.com>
2022-07-16 09:13:05 -0500

    EstimateLayout

--- client/src/components/LandingPage/Services.jsx
+++ client/src/components/LandingPage/Services.jsx
@@ -14,19 +14,66 @@ function Services(props) {
 
       <main>
 
-      <aside className='ServiceFraise'>
-          <p>Helping businesses with</p>
-          <p>mobile websites and apps</p>
-      </aside>
+        <aside className='ServiceFraise'>
+            <p>Helping businesses with</p>
+            <p>mobile websites and apps</p>
+        </aside>
 
-        <header>
-          <h2>Process</h2>
-          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Cumque laborum consectetur distinctio neque provident? Accusantium saepe, fuga delectus explicabo inventore libero iste quod fugit assumenda aperiam quidem provident vitae sapiente!</p>
-        </header>
+        <section>
+          <Estimate></Estimate>
+        </section>
+
+
+          <header>
+            <h1>My Process</h1>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Cumque laborum consectetur distinctio neque provident? Accusantium saepe, fuga delectus explicabo inventore libero iste quod fugit assumenda aperiam quidem provident vitae sapiente!</p>
+
+
+            <section>
+              <header>
+                <div className="DotIcon"></div>
+                <h2>Phase 1: Design</h2>
+              </header>
+
+              <main>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Cumque laborum consectetur distinctio neque provident? Accusantium saepe, fuga delectus explicabo inventore libero iste quod fugit assumenda aperiam quidem provident vitae sapiente!</p>
+              </main>
+          
+            </section>
+
+
+            <section>
+              <header>
+                <div className="DotIcon"></div>
+                <h2>Phase 1: Design</h2>
+              </header>
+
+              <main>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Cumque laborum consectetur distinctio neque provident? Accusantium saepe, fuga delectus explicabo inventore libero iste quod fugit assumenda aperiam quidem provident vitae sapiente!</p>
+              </main>
+          
+            </section>
+
+
+            <section>
+              <header>
+                <div className="DotIcon"></div>
+                <h2>Phase 1: Design</h2>
+              </header>
+
+              <main>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Cumque laborum consectetur distinctio neque provident? Accusantium saepe, fuga delectus explicabo inventore libero iste quod fugit assumenda aperiam quidem provident vitae sapiente!</p>
+              </main>
+          
+            </section>
+
+        
+
+
+          </header>
 
   
 
-        <Estimate></Estimate>
 
         {/* <Subscription></Subscription> */}
 
